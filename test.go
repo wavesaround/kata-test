37644e437fac6e9902bf707655747d3365d2e17b
wavesaround <artem.mustaev@gmail.com>
2024-08-30 00:50:24 +0300

    add some validation and panic

--- test.go
+++ test.go
@@ -4,11 +4,81 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Пожалуйста, введите одно арифметическое выражение с двумя числами. \nДля получения результата нажмите Enter")
+
+	text, _ := reader.ReadString('\n')
+
+	text = strings.TrimSpace(text)
+
+	f := func(r rune) bool {
+		return r == '+' || r == '-' || r == '*' || r == '/'
+	}
+
+	if !strings.ContainsFunc(text, f) {
+		panic("arithmetic operator not found")
+	}
+
+	if findOperator(text) > 1 {
+		panic("more than 1 arithmetic operator")
+	}
+
+}
+
+func findOperator (str string) int {
+	operators := func(r rune) bool {
+		return r == '+' || r == '-' || r == '*' || r == '/' || r == '='
+	}
+	count := 0
+	for i := 0; i < len(str); i++ {
+		if strings.ContainsFunc(string(str[i]), operators) {
+			count++
+		}
+    }
+	return count
+}
+
+func prepareString(str string) string {
+	strNew := strings.TrimSpace(str)
+	strNew = strings.ToUpper(strNew)
+	fmt.Println(strNew)
+	return strNew
+}
+
+func romeToArab(x string) int {
+
+	arabToRome := map[string] int {
+		"I":  1,
+		"II":  2,
+		"III":  3,
+		"IV":  4,
+		"V":  5,
+		"VI":  6,
+		"VII":  7,
+		"VIII":  8,
+		"IX":  9,
+		"X":  10,
+		"XI":  11,
+		"XII":  12,
+		"XIII":  13,
+		"XIV":  14,
+		"XV":  15,
+		"XVI":  16,
+		"XVII":  17,
+		"XVIII":  18,
+		"XIX":  19,
+		"XX": 20,
+	}
+	return arabToRome[x]
+} 
+
 func arabToRome(x int) string {
+
 	arabToRome := map[int]string{
 		1:  "I",
 		2:  "II",
@@ -32,16 +102,4 @@ func arabToRome(x int) string {
 		20: "XX",
 	}
 	return arabToRome[x]
-}
-
-func main() {
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Пожалуйста, введите выражениe")
-	text, _ := reader.ReadString('\n')
-	text = strings.TrimSpace(text)
-
-	toNumber, _ := strconv.Atoi(text)
-	fmt.Println(arabToRome(toNumber))
-
 }
